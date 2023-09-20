011a7560a12296bcc83bd77175289311e86356ae
lirikape <lirikape@gmail.com>
2023-09-20 12:45:57 +0300

    hw

---
+++ src/js/01-gallery.js
@@ -0,0 +1,24 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Change code below this line
+
+const galleryList = document.querySelector('.gallery');
+const galleryItemsHTML = galleryItems.map((item) => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+  </li>
+`).join('');
+
+galleryList.innerHTML = galleryItemsHTML;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+
+console.log(galleryItems);
