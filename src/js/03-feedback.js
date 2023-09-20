5424ee6d0225b10744c5cb33c660ac456e3b7b07
lirikape <lirikape@gmail.com>
2023-09-20 15:01:17 +0300

    hw8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageInput = feedbackForm.querySelector('textarea[name="message"]');
+const localStorageKey = 'feedback-form-state';
+
+function saveFormData() {
+    const formData = {
+        email: emailInput.value,
+        message: messageInput.value,
+    };
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+}
+
+function loadFormData() {
+    const savedData = localStorage.getItem(localStorageKey);
+    if (savedData) {
+        const formData = JSON.parse(savedData);
+        emailInput.value = formData.email;
+        messageInput.value = formData.message;
+    }
+}
+
+feedbackForm.addEventListener('input', throttle(saveFormData, 500));
+window.addEventListener('load', loadFormData);
+
+feedbackForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const formData = {
+        email: emailInput.value,
+        message: messageInput.value,
+    };
+    console.log(formData);
+    localStorage.removeItem(localStorageKey);
+    emailInput.value = '';
+    messageInput.value = '';
+});
